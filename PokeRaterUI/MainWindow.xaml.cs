509a1140cec80192173a48943576331dd7b60ace
Phil Oats <phillip.oats@gmail.com>
2016-03-23 14:35:33 +0000

    Create initial UI

--- PokeRaterUI/MainWindow.xaml.cs
+++ PokeRaterUI/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PokeRater;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PokeRaterUI
 {
@@ -23,7 +12,7 @@ namespace PokeRaterUI
         public MainWindow()
         {
             InitializeComponent();
-            /* Phil do something */
+            DataContext = new RaterViewModel(new DualEloPlayer());
         }
     }
 }
---
+++ PokeRaterUI/RaterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using PokeRater;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace PokeRaterUI
+{
+    public class RaterViewModel : ViewModelBase { 
+
+        private List<Pokemon> _currentSelection;
+        public List<Pokemon> CurrentSelection
+        {
+            get { return _currentSelection; }
+            set { _currentSelection = value; RaisePropertyChangedEvent("CurrentSelection"); }
+        }
+
+        private IGamePlayer player;
+
+        public RaterViewModel(IGamePlayer player)
+        {
+            this.player = player;
+            UpdateSelection();
+        }
+
+        private void UpdateSelection()
+        {
+            CurrentSelection = new List<Pokemon>(player.GetNewSelection());
+        }
+
+        private ICommand _choosePokemonCommand;
+        public ICommand ChoosePokemonCommand
+        {
+            get
+            {
+                return _choosePokemonCommand ?? (_choosePokemonCommand = new CommandHandler<Pokemon>(p => ChoosePokemon(p), true));
+            }
+        }
+
+        public void ChoosePokemon(Pokemon choice)
+        {
+            player.PlayGame(CurrentSelection.ToArray(), choice);
+            UpdateSelection();
+        }
+    }
+}
