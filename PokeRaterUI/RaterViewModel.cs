30a1da9bdfed59ab4688353dee94bcc2ef32c9c6
Phil Oats <phillip.oats@gmail.com>
2016-03-25 13:27:29 +0000

    Add sprites to UI

--- PokeRaterUI/RaterViewModel.cs
+++ PokeRaterUI/RaterViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using PokeRater;
+using PokeRaterUI.Properties;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace PokeRaterUI
 {
-    public class RaterViewModel : ViewModelBase { 
-
-        private List<Pokemon> _currentSelection;
-        public List<Pokemon> CurrentSelection
+    public class RaterViewModel : ViewModelBase
+    {
+        private List<PokemonSprite> _currentSelection;
+        public List<PokemonSprite> CurrentSelection
         {
             get { return _currentSelection; }
             set { _currentSelection = value; RaisePropertyChangedEvent("CurrentSelection"); }
@@ -18,12 +22,13 @@ namespace PokeRaterUI
         public RaterViewModel(IGamePlayer player)
         {
             this.player = player;
+            CurrentSelection = new List<PokemonSprite>();
             UpdateSelection();
         }
 
         private void UpdateSelection()
         {
-            CurrentSelection = new List<Pokemon>(player.GetNewSelection());
+            CurrentSelection = player.GetNewSelection().Select(p => new PokemonSprite(p, new BitmapImage(new Uri("pack://application:,,,/PokemonPixelSprites/" + p.DexNum + ".png")))).ToList();
         }
 
         private ICommand _choosePokemonCommand;
@@ -31,14 +36,26 @@ namespace PokeRaterUI
         {
             get
             {
-                return _choosePokemonCommand ?? (_choosePokemonCommand = new CommandHandler<Pokemon>(p => ChoosePokemon(p), true));
+                return _choosePokemonCommand ?? (_choosePokemonCommand = new CommandHandler<PokemonSprite>(p => ChoosePokemon(p.Poke), true));
             }
         }
 
         public void ChoosePokemon(Pokemon choice)
         {
-            player.PlayGame(CurrentSelection.ToArray(), choice);
+            player.PlayGame(CurrentSelection.Select(p => p.Poke).ToArray(), choice);
             UpdateSelection();
+        }
+
+        public class PokemonSprite
+        {
+            public BitmapImage Image { get; set; }
+            public Pokemon Poke { get; set; }
+
+            public PokemonSprite(Pokemon p, BitmapImage b)
+            {
+                this.Image = b;
+                this.Poke = p;
+            }
         }
     }
 }
